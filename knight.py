16b6da9787d6d8a2be098e73ba54bb47ac0ec0a7
daniarlert <daniarlert@gmail.com>
2022-09-17 18:41:05 +0200

    feat: display text

--- knight.py
+++ knight.py
@@ -15,31 +15,48 @@ COLORS = [
 
 
 class Square():
-    def __init__(self, file: int, rank: int, size: float, weight: int) -> None:
+    def __init__(self, font: pygame.font, file: int, rank: int, size: float, weight: int) -> None:
+        self.font = font
+
         self.file = file
         self.rank = rank
         self.size = size
         self.weight = weight
 
     def draw(self, screen: pygame.Surface) -> None:
-        """Create a pygame.Rect at (x, y) position."""
+        """Draw a pygame.Rect at (x, y) position."""
+        x = self.file * self.size
+        y = self.rank * self.size
         square = pygame.Rect(
-            self.file * self.size,
-            self.rank * self.size,
+            x,
+            y,
             self.size,
             self.size)
 
         pygame.draw.rect(screen, COLORS[self.weight], square)
 
+        self.text = self.font.render(f'{self.weight}', True, WHITE)
+        screen.blit(
+            self.text,
+            (x + self.size/2,
+             y + self.size/2 - self.font.get_height()/2))
+
 
 class Board():
-    def __init__(self, screen: pygame.Surface) -> None:
+    def __init__(self, screen: pygame.Surface, font: pygame.font) -> None:
         """Initializes a 8x8 chessboard."""
         self.screen = screen
+        self.font = font
 
         self.square_size = self.screen.get_height() / 8
         self.board = [
-            [Square(file, rank, self.square_size, random.choice(range(5))) for file in range(8)] for rank in range(8)]
+            [Square(self.font, file, rank, self.square_size, 1) for file in range(8)] for rank in range(8)]
+        self._select_random_square()
+
+    def _select_random_square(self) -> None:
+        file = random.choice(range(8))
+        rank = random.choice(range(8))
+        self.board[file][rank].weight = 0
 
     def draw(self) -> None:
         """Draw chessboard."""
@@ -63,7 +80,11 @@ class Knight():
         self.screen = pygame.display.set_mode(
             (self.screen_width, self.screen_height))
 
-        self.board = Board(self.screen)
+        # TODO: use a better font
+        # TODO: improve font size
+        self.font = pygame.font.Font(None, 24)
+
+        self.board = Board(self.screen, self.font)
 
         self.ck = pygame.time.Clock()
 
